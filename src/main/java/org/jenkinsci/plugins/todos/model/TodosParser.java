335c2a0d20d4a18bf154e0dcbc71c1251a0eb471
Michal Turek <mixalturek@users.sf.net>
2013-11-19 04:31:31 +0100

    Headers of files
    - Note "This file is part of TODOs Plugin (Jenkins CI)." added to header of all source files.

--- src/main/java/org/jenkinsci/plugins/todos/model/TodosParser.java
+++ src/main/java/org/jenkinsci/plugins/todos/model/TodosParser.java
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2013 Michal Turek
+ * This file is part of TODOs Plugin (Jenkins CI).
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
